fd5ed828980e1c97cd24fdc34c6e9e4e08343ce3
MAJIAXIT <vitaromat9@gmail.com>
2024-03-25 23:12:19 +0300

    Add comments

--- internal/service/users.go
+++ internal/service/users.go
@@ -23,6 +23,20 @@ func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Get user by access token method.
+// Expected header:
+// Authorization : Bearer <Access token>.
+// Response:
+// Error message or user data:
+// id : user id;
+// email : user email;
+// name : user name;
+// patronymic : user patronymic;
+// surname : user surname;
+// role_id : id of user role;
+// dep_id : id of user department.
+// Response codes:
+// 200, 400, 401, 404, 405.
 func UsersGetHandler(w http.ResponseWriter, r *http.Request) {
 	authorized, err := auth.CheckUserAuthorized(r)
 	if err != nil {
