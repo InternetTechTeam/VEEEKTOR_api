3df2ba48720ec0ffc14d0ae0012b05ba5f5a5490
MAJIAXIT <vitaromat9@gmail.com>
2024-03-25 23:02:41 +0300

    Implement user get by request

--- internal/service/users.go
+++ internal/service/users.go
@@ -13,6 +13,47 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		UsersGetHandler(w, r)
+	default:
+		e.ResponseWithError(w, r, http.StatusMethodNotAllowed,
+			e.ErrOnlyGetAllowed)
+	}
+}
+
+func UsersGetHandler(w http.ResponseWriter, r *http.Request) {
+	authorized, err := auth.CheckUserAuthorized(r)
+	if err != nil {
+		e.ResponseWithError(
+			w, r, http.StatusUnauthorized, err)
+		return
+	}
+
+	if !authorized {
+		e.ResponseWithError(
+			w, r, http.StatusUnauthorized, e.ErrTokenExpired)
+		return
+	}
+
+	userId, err := auth.GetUserIdFromRequest(r)
+	if err != nil {
+		e.ResponseWithError(w, r, http.StatusBadRequest, err)
+		return
+	}
+
+	user, err := models.GetUserById(userId)
+	if err != nil {
+		e.ResponseWithError(w, r, http.StatusNotFound, e.ErrUserNotFound)
+		return
+	}
+	user.Password = ""
+
+	jsonBytes, _ := json.Marshal(user)
+	w.Write(jsonBytes)
+}
+
 // Authorization, authentication logic.
 // Expected body:
 // "email" 	  : user email (4-64 symbols),
